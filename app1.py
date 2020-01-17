4ad838279da35c3ad91078e5cd0c9dd528a18c46
lidia01 <57924592+lidia01@users.noreply.github.com>
2020-01-17 16:20:53 -0500

    agregue app1.py

---
+++ app1.py
@@ -0,0 +1,72 @@
+#01
+import libreria1
+#input
+nombre=libreria1.pedir_nombre("ingrese nombre:")
+tipo=libreria1.pedir_entero("ingrese tipo:", 0, 0)
+prestamo=libreria1.pedir_real("ingrese prestamo:")
+
+#processing
+descuento=(libreria1 * prestamo)
+
+#output
+libreria1.reporte_prestamo(nombre, prestamo, descuento)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#11
+import libreria1
+#input
+nombre=libreria1.pedir_nombre("ingrese nombre:")
+tipo=libreria1.pedir_entero("ingrese tipo:", 0, 0)
+prestamo=libreria1.pedir_real("ingrese prestamo:")
+
+#processing
+descuento=(libreria1 * prestamo)
+
+#output
+libreria1.reporte_prestamo(nombre, prestamo, descuento)
+
+
+#12
+import libreria1
+nombre="lid"
+ms=libreria1.lidi(nombre)
+print(lidi)
+
+
+
+
+
+
