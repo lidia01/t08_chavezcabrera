a36bd47382e145c646614dec8acb3360a1d95f76
lidia01 <57924592+lidia01@users.noreply.github.com>
2020-01-17 16:21:30 -0500

    agregue app2.py

---
+++ app2.py
@@ -0,0 +1,11 @@
+#02
+import libreria1
+#input
+color=libreria1.pedir_color("ingrese color:")
+emergencia=libreria1.pedir_mensaje("ingrese mensaje:")
+
+#processing
+rojo=color+emergencia
+
+#output
+libreria1.reporte_de_emergencia(color, emergencia)
